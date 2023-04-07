5340776f80eee240a73851a93dc0b4c0a07275f0
Saba Mchedlishvili <sabamchedlishvili17@gmail.com>
2023-04-07 15:17:40 +0400

    add second activity

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.homework1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fbBtn = findViewById<Button>(R.id.fb_button)
+        val googleBtn = findViewById<Button>(R.id.google_button)
+        val registerBtn = findViewById<Button>(R.id.register_button)
+
+        registerBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        fbBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        googleBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/homework1/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.homework1
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
